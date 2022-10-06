e3feebcb028dfb475345c8a6d936a9db296be477
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-10-06 18:14:50 +0530

    Added Binary search problem

---
+++ src/com/bridgelabz/BinarySearch.java
@@ -0,0 +1,46 @@
+package com.bridgelabz;
+
+public class BinarySearch 
+{
+	// Returns index of x if it is present in arr[],
+    
+    static int binarySearch(String[] arr, String x)
+    {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+ 
+            int res = x.compareTo(arr[m]);
+ 
+            // Check if x is present at mid
+            if (res == 0)
+                return m;
+ 
+            // ignore left half
+            if (res > 0)
+                l = m + 1;
+ 
+            // If x is smaller, ignore right half
+            else
+                r = m - 1;
+        }
+ 
+        return -1;
+    }
+ 
+    // Driver method to test above
+    public static void main(String []args)
+    {
+        String[] arr = { "Bob", "adam", "jack", "Evan"};
+        String x = "Jack";
+        int result = binarySearch(arr, x);
+ 
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at "
+                              + "index " + result);
+    }
+}
+
+
